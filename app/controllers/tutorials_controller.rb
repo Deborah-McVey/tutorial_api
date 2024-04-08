f4c5a18258a759fb0cd8154cc98f52737b50d372
Deborah McVey <deborah.mcvey@gmail.com>
2024-04-07 21:26:45 -0400

    everything good in Postman, work on show/find by title/keyword

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,16 +1,17 @@
 class TutorialsController < ApplicationController
-  before_action :set_tutorial, only: %i[ show update destroy ]
+  before_action :set_tutorial, only: %i[ update destroy ]
 
   # GET /tutorials
   def index
     @tutorials = Tutorial.all
 
-    render json: @tutorials
+    render json: @tutorials, status: :ok
   end
 
   # GET /tutorials/1
   def show
-    render json: @tutorial
+    @tutorial = Tutorial.find(params[:id, :title])
+    render json: @tutorial, status: :ok
   end
 
   # POST /tutorials
@@ -27,7 +28,7 @@ class TutorialsController < ApplicationController
   # PATCH/PUT /tutorials/1
   def update
     if @tutorial.update(tutorial_params)
-      render json: @tutorial
+      render json: @tutorial, status: :ok
     else
       render json: @tutorial.errors, status: :unprocessable_entity
     end
@@ -35,7 +36,11 @@ class TutorialsController < ApplicationController
 
   # DELETE /tutorials/1
   def destroy
-    @tutorial.destroy!
+    if @tutorial.destroy!
+    render json: nil, status: :ok
+        else
+          render json: @post.errors, status: :unprocessable_entity
+        end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  resources :tutorials
-  resources :tutorial
+  # localhost:3000/api/tutorials
+  post '/api/tutorials', to: 'tutorials#create'
+  # localhost:3000/api/tutorials
+  get '/api/tutorials', to: 'tutorials#index'
+  # localhost:3000/api/tutorials/1
+  get '/api/tutorials/:id', to: 'tutorials#show'
+  # localhost:3000/api/tutorials/1
+  put '/api/tutorials/:id', to: 'tutorials#update'
+  # localhost:3000/api/tutorials/1
+  delete '/api/tutorials/:id', to: 'tutorials#destroy'
+  # localhost:3000/api/tutorials
+  delete '/api/tutorials', to: 'tutorials#destroy'
+  # localhost:3000/api/tutorials?title=[keyword]
+  get '/api/tutorials?title=[keyword]', to: 'tutorials#title[keyword]' #show action
+  #resources :tutorials
 end
